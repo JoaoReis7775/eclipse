98b88debfff619499032b2b0ccfbb0d51ea0dacb
JoaoReis7775 <60515443+JoaoReis7775@users.noreply.github.com>
2020-04-09 17:13:17 -0300

    Add arquivos via Upload

---
+++ Hello Github/src/br/com/joaoreis7775/git/HelloGit.java
@@ -0,0 +1,10 @@
+package br.com.joaoreis7775.git;
+
+//exemplo de integração do eclipse com o Github
+
+public class HelloGit {
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+	}
+
+}
